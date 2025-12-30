a9369a1edd72d37b7719db8bf45a542ef42d104c
nas7062 <nas7062@naver.com>
2025-12-30 13:15:06 +0900

    feat : header serachBar 구분

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,10 +8,14 @@ interface HeaderProps {
 
 export default function Header({ isSearch = false }: HeaderProps) {
   return (
-    <div className="flex items-center justify-between gap-2 w-full">
+    <div className="flex items-center justify-between gap-4  w-full">
       <LogoImage />
-      {isSearch && <SearchInput />}
-      <MenuBtns />
+      {isSearch && (
+        <div className="flex-1">
+          <SearchInput />
+        </div>
+      )}
+      <MenuBtns isSearch={isSearch} />
     </div>
   );
 }
--- app/components/MenuBtns.tsx
+++ app/components/MenuBtns.tsx
@@ -1,18 +1,25 @@
 import { Search, ShoppingCart, UserRound } from "lucide-react";
+import Link from "next/link";
 
-export default function MenuBtns() {
+interface menuProps {
+  isSearch?: boolean;
+}
+
+export default function MenuBtns({ isSearch = false }: menuProps) {
   const btnCss = `hover:bg-slate-100 p-1.5 rounded-lg cursor-pointer transition-color duration-250`;
   return (
     <div className="flex gap-4">
-      <button className={btnCss} aria-label="검색 버튼">
-        <Search size={25} />
-      </button>
-      <button className={btnCss} aria-label="카트 버튼">
+      {!isSearch && (
+        <Link href={"/search"} className={btnCss} aria-label="검색 버튼">
+          <Search size={25} />
+        </Link>
+      )}
+      <Link href={"/cart"} className={btnCss} aria-label="카트 버튼">
         <ShoppingCart size={25} />
-      </button>
-      <button className={btnCss} aria-label="내 정보 버튼">
+      </Link>
+      <Link href={"/my"} className={btnCss} aria-label="내 정보 버튼">
         <UserRound size={25} />
-      </button>
+      </Link>
     </div>
   );
 }
