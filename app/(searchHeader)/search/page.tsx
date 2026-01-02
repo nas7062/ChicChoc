8e9463edbb79c235e58211a56887d5bdc34ea6d5
nas7062 <nas7062@naver.com>
2026-01-02 13:01:54 +0900

    style : css 수정

--- app/(searchHeader)/search/page.tsx
+++ app/(searchHeader)/search/page.tsx
@@ -13,8 +13,8 @@ export default function Page() {
     <div className="flex gap-1 ">
       {recentSearch.map((search) => (
         <div key={search} className="relative">
-          <p key={search} className=" text-sm text-gray-500 border border-gray-300 rounded-xl pl-4 pr-6 py-1 hover:bg-gray-100 cursor-pointer">{search}</p>
-          <X className="absolute right-1.5 top-1.5 w-4 h-4 text-gray-400 hover:text-gray-700 cursor-pointer" />
+          <p key={search} className=" text-xs text-gray-500 border border-gray-300 rounded-xl pl-4 pr-6 py-1 hover:bg-gray-100 cursor-pointer">{search}</p>
+          <X className="absolute right-1.5 top-[5px] w-4 h-4 text-gray-400 hover:text-gray-700 cursor-pointer" />
         </div>
       ))}
     </div>
@@ -27,8 +27,8 @@ export default function Page() {
       <div className="flex flex-col flex-wrap gap-1 w-full h-60">
         {popularSearch.map((search, idx) => (
           <div key={search} className="flex items-center gap-2 hover:bg-gray-100 p-1 rounded-md cursor-pointer w-1/2">
-            <p className="text-sm text-gray-500 w-8 h-8 flex items-center justify-center rounded-md bg-gray-100">{idx + 1}</p>
-            <p className="text-sm text-gray-500 text-nowrap">{search}</p>
+            <p className="text-xs text-gray-500 w-8 h-8 flex items-center justify-center rounded-md bg-gray-100">{idx + 1}</p>
+            <p className="text-xs text-gray-500 text-nowrap">{search}</p>
           </div>
         ))}
       </div>
--- app/components/SearchInput.tsx
+++ app/components/SearchInput.tsx
@@ -5,7 +5,7 @@ export default function SearchInput() {
     <div className="relative ">
       <input
         type="search"
-        className="border border-gray-300 rounded-xl w-full h-8"
+        className="border border-gray-300 rounded-xl w-full h-8 pl-10 pr-4 text-sm"
       />
       <Search className="absolute left-2 top-1 text-gray-500" size={24} />
     </div>
