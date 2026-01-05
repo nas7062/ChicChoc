e466528d6a6956931893db1a08c9f0abe2dafb54
nas7062 <nas7062@naver.com>
2026-01-05 14:36:49 +0900

    chore : index.ts 주석처리

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      email: "test@test.com",
-      name: "Kim",
-    },
-  });
+// async function main() {
+//   const user = await prisma.user.create({
+//     data: {
+//       email: "test@test.com",
+//       name: "Kim",
+//     },
+//   });
 
-  console.log("created:", user);
+//   console.log("created:", user);
 
-  const users = await prisma.user.findMany();
-  console.log("all users:", users);
-}
+//   const users = await prisma.user.findMany();
+//   console.log("all users:", users);
+// }
 
-main()
-  .catch(console.error)
-  .finally(() => prisma.$disconnect());
+// main()
+//   .catch(console.error)
+//   .finally(() => prisma.$disconnect());
