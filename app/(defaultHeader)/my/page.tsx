1edaaec7a8ec4ac3e277b60d70fe170b0e79c458
nas7062 <nas7062@naver.com>
2026-01-06 21:27:51 +0900

    feat :  로그아웃 버튼 생성

--- app/(defaultHeader)/my/page.tsx
+++ app/(defaultHeader)/my/page.tsx
@@ -1,19 +1,31 @@
 "use client";
-import { BadgeRussianRuble, List, SquareChartGantt } from "lucide-react";
+import { BadgeRussianRuble, List, LogOut, SquareChartGantt } from "lucide-react";
+import { signOut } from "next-auth/react";
 
 const MyList = [
   { label: "주문 내역", icon: List },
   { label: "리뷰", icon: SquareChartGantt },
   { label: "포인트", icon: BadgeRussianRuble },
+  {
+    label: "로그아웃",
+    icon: LogOut,
+    action: "logout",
+  },
 ];
 
 export default function MyPage() {
+  const handleClick = (item: (typeof MyList)[number]) => {
+    if (item.action === "logout") {
+      signOut({ callbackUrl: "/auth/signin" });
+    }
+  };
+
   return (
     <div className="w-full">
       <div className="flex flex-col gap-2 justify-center items-center w-full h-40">
         <p className="font-semibold">민석</p>
         <p className="text-xs text-gray-500">nas7062@naver.com</p>
-        <button className="text-white bg-blue-400 text-sm hover:bg-blue-500 px-4 py-1 rounded-2xl transition-colors duration-300  cursor-pointer">
+        <button className="text-white bg-blue-400 text-sm hover:bg-blue-500 px-4 py-1 rounded-2xl transition-colors duration-300 cursor-pointer">
           내 정보 수정
         </button>
       </div>
@@ -26,6 +38,7 @@ export default function MyPage() {
           return (
             <div
               key={list.label}
+              onClick={() => handleClick(list)}
               className="flex items-center gap-3 px-4 py-3 hover:bg-gray-100 cursor-pointer"
             >
               <Icon size={20} className="text-gray-700" />
