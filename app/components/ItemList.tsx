d969086c7227001ed609eb809ea5874e4c3750c7
nas7062 <nas7062@naver.com>
2026-01-09 10:52:57 +0900

    chore : 다시 코드 변경

--- app/components/ItemList.tsx
+++ app/components/ItemList.tsx
@@ -1,26 +1,22 @@
-import { createClient } from "@/lib/supabase/server";
+import { Product } from "../type";
 import { Item } from "./Item";
 
 export default async function ItemList() {
-  const supabase = await createClient();
-
-  const { data: items, error } = await supabase
-    .from("Product")
-    .select("*")
-    .order("createdAt", { ascending: false })
-    .limit(50);
-
-  if (error) {
-    throw new Error(error.message);
+  const res = await fetch(
+    `/api/products`,
+    { cache: "no-store" }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
   }
+
+  const { items } = await res.json();
 
   return (
     <div className="flex flex-col gap-4">
       <h2 className="font-semibold">당신을 위한 추천 아이템</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-1">
-        {items?.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
+      <div className="grid  grid-cols-2 sm:grid-cols-3 gap-1">
+        {items && items.map((item: Product) => <Item item={item} key={item.id} />)}
       </div>
     </div>
   );
