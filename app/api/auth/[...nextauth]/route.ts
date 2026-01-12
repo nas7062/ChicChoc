5aac21bcdd3b76d7818814291116bb57b6d8b325
nas7062 <nas7062@naver.com>
2026-01-12 13:03:12 +0900

    fix : build 오류 해결

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,9 @@ export const dynamic = "force-dynamic";
 
 import NextAuth from "next-auth";
 
-async function handler(req: Request, ctx: any) {
+const handler = async (...args: Parameters<typeof NextAuth>) => {
   const { authOptions } = await import("@/lib/auth");
-  return NextAuth(authOptions)(req, ctx);
-}
+  return NextAuth(authOptions)(...args);
+};
 
 export { handler as GET, handler as POST };
