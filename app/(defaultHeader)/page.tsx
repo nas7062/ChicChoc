740feee9bd50acddaf7edb27e87abab62ea21785
nas7062 <nas7062@naver.com>
2026-01-12 13:10:45 +0900

    fix : build 오류 해결

--- app/(defaultHeader)/page.tsx
+++ app/(defaultHeader)/page.tsx
@@ -1,12 +1,13 @@
 export const dynamic = "force-dynamic";
 
 
-import { prisma } from "@/lib/prisma";
+
 import Category from "../components/Category";
 import ItemList from "../components/ItemList";
 import Banner from "./_components/Banner";
 
 export default async function Home() {
+  const { prisma } = await import("@/lib/prisma");
   const items = await prisma.product.findMany({
     where: { isActive: true },
     orderBy: { createdAt: "desc" },
