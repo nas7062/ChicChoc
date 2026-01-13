503f7cbdf7f65b5429ca51d241f1caffdc016189
nas7062 <nas7062@naver.com>
2026-01-13 19:33:56 +0900

    fix : type오류 해결 :

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,9 +1,12 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
+import { unstable_noStore } from "next/cache";
 export async function GET() {
+  unstable_noStore();
   const { prisma } = await import("@/lib/prisma");
   const session = await getServerSession(authOptions);
   console.log("session:", session);
